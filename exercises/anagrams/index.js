974d767d6cc8c13030f2419c05b53ea474f1ddfe
RMH <rory.hennessey@gmail.com>
2020-05-05 10:01:41 -0400

    first anagram solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -1,5 +1,5 @@
 // --- Directions
-// Check to see if two provided strings are anagrams of eachother.
+// Check to see if two provided strings are anagrams of each other.
 // One string is an anagram of another if it uses the same characters
 // in the same quantity. Only consider characters, not spaces
 // or punctuation.  Consider capital letters to be the same as lower case
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false
+  }
+
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+
+}
+
+// First build a helper function that will go through each string
+function buildCharMap(str) {
+  const charMap = {};
+
+  // use regex to get rid of spaces & non-letters + bring all down to lowercase
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    // we need the || 1 for the first instance; otherwise it will fail
+    charMap[char] = charMap[char] + 1 || 1
+  }
+
+  return charMap
+}
 
 module.exports = anagrams;
