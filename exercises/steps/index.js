21c633a1760e8ada607fef4e5beb2e4ffc0fdd04
Rory Hennessey <rory.hennessey@gmail.com>
2020-05-07 13:37:07 -0400

    added steps solution (non-recursive)

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,21 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+	// loop through every row to create a stair
+	for (let row = 0; row < n; row++) {
+		let stair = "";
+
+		// loop thru every column
+		for (let column = 0; column < n; column++) {
+			if (column <= row) {
+				stair += "#";
+			} else {
+				stair += " ";
+			}
+		}
+		console.log(stair);
+	}
+}
 
 module.exports = steps;
